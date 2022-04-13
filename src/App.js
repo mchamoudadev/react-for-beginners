3078eaffcca654c2288743edd04e471c3e440375
mc hamouda <mchamuuda@gmail.com>
2022-04-13 11:02:12 +0300

    week two

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
 
+import styles from './Div.module.css';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const user = "Kalid";
+
+    const students = [
+        {
+            name: "Kalid",
+            class: "CA181"
+        },
+        {
+            name: "Fartun",
+            class: "CA182"
+        },
+        {
+            name: "Yusuf",
+            class: "CA188"
+        },
+        {
+            name: "Kalthuma",
+            class: "CA188"
+        },
+    ];
+
+    const divStyle = {
+        backgroundColor: "green",
+        padding: 10,
+        borderRadius: 15,
+        color: "white"
+    };
+
+    function submitForm(event) {
+        event.preventDefault();
+        console.log("submitted");
+        let username = document.getElementById("username");
+        console.log(username.value);
+        if (username.value.length < 3) {
+            alert("make sure to have 3 or greater than");
+        }
+    }
+
+
+    return (
+        <div className={styles.something} >
+            {/* <h1>Hello {user} age {2022 - 2004}</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, rem?</p> */}
+
+            {
+                // rendreing List
+                // students.map((student, index) => (
+                //     <div key={index} className={styles.card}>
+                //         <li>{student.name}</li>
+                //         <li>{student.class}</li>
+                //         <button onClick={() => alert("Clicked")}>See More</button>
+                //     </div>
+                // ))
+
+
+            }
+
+            <form onSubmit={submitForm}>
+                <input id="username" type="text" />
+                <button type="submit">Submnit</button>
+            </form>
+
+
+        </div>
+
+
+    );
 }
 
 export default App;
